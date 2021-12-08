d28754edc6cd8f9fe64321111cfff7da7ce2ff3d
rinakCP <93871902+rinakCP@users.noreply.github.com>
2021-12-08 10:03:29 +0200

    Add files via upload

---
+++ regions/ap/apOnboarding.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "setting up default project $1"
+gcloud config set project $1
+echo "Enabling Deployment Manager APIs, which you will need for this deployment."
+gcloud services enable deploymentmanager.googleapis.com
+sh ../../onboarding.sh ap $1
