55f6f5ad99c2619909f82071c1ff82da85cd3e65
Rina Kochirgan <rinak@checkpoint.com>
2021-12-30 15:12:00 +0200

    change deleting process

--- offboarding.sh
+++ offboarding.sh
@@ -10,21 +10,12 @@ gcloud config set project "$PROJECT"
 echo "Enabling Deployment Manager APIs, which you will need for the offboarding."
 gcloud services enable deploymentmanager.googleapis.com
 
-# service account deletion
-serviceAccount=$(gcloud iam service-accounts list --filter="name.scope(service account):$SERVICE_ACCOUNT_NAME" 2>&1)
-if [[ ! "$serviceAccount" =~ "0 items" ]]; then
-  serviceAccount=$(gcloud iam service-accounts delete "$SERVICE_ACCOUNT_NAME"@"$PROJECT".iam.gserviceaccount.com)
-  if [[ "$serviceAccount" =~ "ERROR" ]]; then
-    echo "could not delete existing service account "$SERVICE_ACCOUNT_NAME" "
-  fi
-fi
-
-# topic deletion
-topic=$(gcloud pubsub topics list --filter="name.scope(topic):"$TOPIC_NAME"" 2>&1)
-if [[ ! "$topic" =~ "0 items" ]]; then
-  topic=$(gcloud pubsub topics delete "$TOPIC_NAME")
-  if [[ "$topic" =~ "ERROR" ]]; then
-    echo "could not delete existing topic "$TOPIC_NAME" "
+# sink deletion
+sink=$(gcloud logging sinks list --filter="name.scope(sink):"$SINK_NAME"" 2>&1)
+if [[ ! "$sink" =~ "0 items" ]]; then
+  sink=$(gcloud logging sinks delete "$SINK_NAME")
+  if [[ "$sink" =~ "ERROR" ]]; then
+    echo "could not delete existing sink "$SINK_NAME" "
   fi
 fi
 
@@ -37,12 +28,21 @@ if [[ ! "$pubsubSubscription" =~ "0 items" ]]; then
   fi
 fi
 
-# sink deletion
-sink=$(gcloud logging sinks list --filter="name.scope(sink):"$SINK_NAME"" 2>&1)
-if [[ ! "$sink" =~ "0 items" ]]; then
-  sink=$(gcloud logging sinks delete "$SINK_NAME")
-  if [[ "$sink" =~ "ERROR" ]]; then
-    echo "could not delete existing sink "$SINK_NAME" "
+# topic deletion
+topic=$(gcloud pubsub topics list --filter="name.scope(topic):"$TOPIC_NAME"" 2>&1)
+if [[ ! "$topic" =~ "0 items" ]]; then
+  topic=$(gcloud pubsub topics delete "$TOPIC_NAME")
+  if [[ "$topic" =~ "ERROR" ]]; then
+    echo "could not delete existing topic "$TOPIC_NAME" "
+  fi
+fi
+
+# service account deletion
+serviceAccount=$(gcloud iam service-accounts list --filter="name.scope(service account):$SERVICE_ACCOUNT_NAME" 2>&1)
+if [[ ! "$serviceAccount" =~ "0 items" ]]; then
+  serviceAccount=$(gcloud iam service-accounts delete "$SERVICE_ACCOUNT_NAME"@"$PROJECT".iam.gserviceaccount.com)
+  if [[ "$serviceAccount" =~ "ERROR" ]]; then
+    echo "could not delete existing service account "$SERVICE_ACCOUNT_NAME" "
   fi
 fi
 
