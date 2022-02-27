f5975978174bd20aedc5c8af459bda325b8c1a67
israelame <israelam@checkpoint.com>
2022-02-27 16:04:28 +0200

    add offboarding to gcp-flowlogs

--- offboarding.sh
+++ offboarding.sh
@@ -4,6 +4,10 @@ TOPIC_NAME="cloudguard-topic"
 SERVICE_ACCOUNT_NAME="cloudguard-logs-authentication"
 SINK_NAME="cloudguard-sink"
 SUBSCRIPTION_NAME="cloudguard-subscription"
+TOPIC_NAME_FL="cloudguard-fl-topic"
+SERVICE_ACCOUNT_NAME_FL="cloudguard-fl-authentication"
+SINK_NAME_FL="cloudguard-fl-sink"
+SUBSCRIPTION_NAME_FL="cloudguard-fl-subscription"
 
 echo "setting up default project "$PROJECT""
 gcloud config set project "$PROJECT"
@@ -18,6 +22,14 @@ if [[ ! "$sink" =~ "0 items" ]]; then
     echo "could not delete existing sink "$SINK_NAME" "
   fi
 fi
+# sink deletion flowlogs
+sink=$(gcloud logging sinks list --filter="name.scope(sink):"$SINK_NAME_FL"" 2>&1)
+if [[ ! "$sink" =~ "0 items" ]]; then
+  sink=$(gcloud logging sinks delete "$SINK_NAME_FL")
+  if [[ "$sink" =~ "ERROR" ]]; then
+    echo "could not delete existing sink "$SINK_NAME_FL" "
+  fi
+fi
 
 # subscription deletion
 pubsubSubscription=$(gcloud pubsub subscriptions list --filter="name.scope(subscription):"$SUBSCRIPTION_NAME"" 2>&1)
@@ -25,6 +37,14 @@ if [[ ! "$pubsubSubscription" =~ "0 items" ]]; then
   pubsubSubscription=$(gcloud pubsub subscriptions delete "$SUBSCRIPTION_NAME")
   if [[ "$pubsubSubscription" =~ "ERROR" ]]; then
     echo "could not delete existing subscription "$SUBSCRIPTION_NAME" "
+  fi
+fi
+# subscription deletion flowlogs
+pubsubSubscription=$(gcloud pubsub subscriptions list --filter="name.scope(subscription):"$SUBSCRIPTION_NAME_FL"" 2>&1)
+if [[ ! "$pubsubSubscription" =~ "0 items" ]]; then
+  pubsubSubscription=$(gcloud pubsub subscriptions delete "$SUBSCRIPTION_NAME_FL")
+  if [[ "$pubsubSubscription" =~ "ERROR" ]]; then
+    echo "could not delete existing subscription "$SUBSCRIPTION_NAME_FL" "
   fi
 fi
 
@@ -36,6 +56,14 @@ if [[ ! "$topic" =~ "0 items" ]]; then
     echo "could not delete existing topic "$TOPIC_NAME" "
   fi
 fi
+# topic deletion flowlogs
+topic=$(gcloud pubsub topics list --filter="name.scope(topic):"$TOPIC_NAME_FL"" 2>&1)
+if [[ ! "$topic" =~ "0 items" ]]; then
+  topic=$(gcloud pubsub topics delete "$TOPIC_NAME_FL")
+  if [[ "$topic" =~ "ERROR" ]]; then
+    echo "could not delete existing topic "$TOPIC_NAME_FL" "
+  fi
+fi
 
 # service account deletion
 serviceAccount=$(gcloud iam service-accounts list --filter="name.scope(service account):$SERVICE_ACCOUNT_NAME" 2>&1)
@@ -43,6 +71,14 @@ if [[ ! "$serviceAccount" =~ "0 items" ]]; then
   serviceAccount=$(gcloud iam service-accounts delete "$SERVICE_ACCOUNT_NAME"@"$PROJECT".iam.gserviceaccount.com)
   if [[ "$serviceAccount" =~ "ERROR" ]]; then
     echo "could not delete existing service account "$SERVICE_ACCOUNT_NAME" "
+  fi
+fi
+# service account deletion flowlogs
+serviceAccount=$(gcloud iam service-accounts list --filter="name.scope(service account):$SERVICE_ACCOUNT_NAME_FL" 2>&1)
+if [[ ! "$serviceAccount" =~ "0 items" ]]; then
+  serviceAccount=$(gcloud iam service-accounts delete "$SERVICE_ACCOUNT_NAME_FL"@"$PROJECT".iam.gserviceaccount.com)
+  if [[ "$serviceAccount" =~ "ERROR" ]]; then
+    echo "could not delete existing service account "$SERVICE_ACCOUNT_NAME_FL" "
   fi
 fi
 
