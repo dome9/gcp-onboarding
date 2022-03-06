60171fdf915423c0ac3411e2ad557050d4c4feba
israelame <israelam@checkpoint.com>
2022-03-06 18:30:15 +0200

    Delete all resources before the creation (in onboardingFlowLogs.sh)

--- onboardingFlowLogs.sh
+++ onboardingFlowLogs.sh
@@ -18,39 +18,17 @@ else
   ENDPOINT="https://gcp-flow-logs-endpoint.logic."$REGION".dome9.com"
 fi
 
-# service account creation
-serviceAccount=$(gcloud iam service-accounts list --filter="name.scope(service account):$SERVICE_ACCOUNT_NAME" 2>&1)
-if [[ ! "$serviceAccount" =~ "0 items" ]]; then
-  serviceAccount=$(gcloud iam service-accounts delete "$SERVICE_ACCOUNT_NAME"@"$PROJECT".iam.gserviceaccount.com)
-  if [[ "$serviceAccount" =~ "ERROR" ]]; then
-    echo "could not delete existing service account "$SERVICE_ACCOUNT_NAME" EXITING WITHOUT DEPLOYMENT"
+# delete all cloudGuard resources if exists
+# sink deletion
+sink=$(gcloud logging sinks list --filter="name.scope(sink):"$SINK_NAME"" 2>&1)
+if [[ ! "$sink" =~ "0 items" ]]; then
+  sink=$(gcloud logging sinks delete "$SINK_NAME")
+  if [[ "$sink" =~ "ERROR" ]]; then
+    echo "could not delete existing sink "$SINK_NAME" EXITING WITHOUT DEPLOYMENT"
     exit 1
   fi
 fi
-serviceAccount=$(gcloud iam service-accounts create $SERVICE_ACCOUNT_NAME --display-name="$SERVICE_ACCOUNT_NAME" 2>&1)
-echo "$serviceAccount"
-if [[ "$serviceAccount" =~ "ERROR" ]]; then
-    echo "could not create service account "$SERVICE_ACCOUNT_NAME" EXITING WITHOUT DEPLOYMENT"
-    exit 1
-fi
-
-# topic creation
-topic=$(gcloud pubsub topics list --filter="name.scope(topic):"$TOPIC_NAME"" 2>&1)
-if [[ ! "$topic" =~ "0 items" ]]; then
-  topic=$(gcloud pubsub topics delete "$TOPIC_NAME")
-  if [[ "$topic" =~ "ERROR" ]]; then
-    echo "could not delete existing topic "$TOPIC_NAME" EXITING WITHOUT DEPLOYMENT"
-    exit 1
-  fi
-fi
-topic=$(gcloud pubsub topics create "$TOPIC_NAME" 2>&1)
-echo "$topic"
-if [[ "$topic" =~ "ERROR" ]]; then
-    echo "could not create topic "$TOPIC_NAME" EXITING WITHOUT DEPLOYMENT"
-    exit 1
-fi
-
-# subscription creation
+# subscription deletion
 pubsubSubscription=$(gcloud pubsub subscriptions list --filter="name.scope(subscription):"$SUBSCRIPTION_NAME"" 2>&1)
 if [[ ! "$pubsubSubscription" =~ "0 items" ]]; then
   pubsubSubscription=$(gcloud pubsub subscriptions delete "$SUBSCRIPTION_NAME")
@@ -59,7 +37,43 @@ if [[ ! "$pubsubSubscription" =~ "0 items" ]]; then
     exit 1
   fi
 fi
+# topic deletion
+topic=$(gcloud pubsub topics list --filter="name.scope(topic):"$TOPIC_NAME"" 2>&1)
+if [[ ! "$topic" =~ "0 items" ]]; then
+  topic=$(gcloud pubsub topics delete "$TOPIC_NAME")
+  if [[ "$topic" =~ "ERROR" ]]; then
+    echo "could not delete existing topic "$TOPIC_NAME" EXITING WITHOUT DEPLOYMENT"
+    exit 1
+  fi
+fi
+# service account deletion
+serviceAccount=$(gcloud iam service-accounts list --filter="name.scope(service account):$SERVICE_ACCOUNT_NAME" 2>&1)
+if [[ ! "$serviceAccount" =~ "0 items" ]]; then
+  serviceAccount=$(gcloud iam service-accounts delete "$SERVICE_ACCOUNT_NAME"@"$PROJECT".iam.gserviceaccount.com)
+  if [[ "$serviceAccount" =~ "ERROR" ]]; then
+    echo "could not delete existing service account "$SERVICE_ACCOUNT_NAME" EXITING WITHOUT DEPLOYMENT"
+    exit 1
+  fi
+fi
 
+# create cloudGuard resources
+# service account creation
+serviceAccount=$(gcloud iam service-accounts create $SERVICE_ACCOUNT_NAME --display-name="$SERVICE_ACCOUNT_NAME" 2>&1)
+echo "$serviceAccount"
+if [[ "$serviceAccount" =~ "ERROR" ]]; then
+    echo "could not create service account "$SERVICE_ACCOUNT_NAME" EXITING WITHOUT DEPLOYMENT"
+    exit 1
+fi
+
+# topic creation
+topic=$(gcloud pubsub topics create "$TOPIC_NAME" 2>&1)
+echo "$topic"
+if [[ "$topic" =~ "ERROR" ]]; then
+    echo "could not create topic "$TOPIC_NAME" EXITING WITHOUT DEPLOYMENT"
+    exit 1
+fi
+
+# subscription creation
 pubsubSubscription=$(gcloud pubsub subscriptions create "$SUBSCRIPTION_NAME" \
                            --topic="$TOPIC_NAME" \
                            --ack-deadline="$ACK_DEADLINE" \
@@ -76,14 +90,6 @@ if [[ "$pubsubSubscription" =~ "ERROR" ]]; then
 fi
 
 # sink creation
-sink=$(gcloud logging sinks list --filter="name.scope(sink):"$SINK_NAME"" 2>&1)
-if [[ ! "$sink" =~ "0 items" ]]; then
-  sink=$(gcloud logging sinks delete "$SINK_NAME")
-  if [[ "$sink" =~ "ERROR" ]]; then
-    echo "could not delete existing sink "$SINK_NAME" EXITING WITHOUT DEPLOYMENT"
-    exit 1
-  fi
-fi
 sink=$(gcloud logging sinks create "$SINK_NAME" pubsub.googleapis.com/projects/"$PROJECT"/topics/"$TOPIC_NAME" \
             --log-filter="$LOG_FILTER" 2>&1)
 echo "$sink"
