07086d46fdd894da1756bcf1370c4b551890263f
yael <yaelel@checkpoint.com>
2023-06-25 18:40:48 +0300

    changed yaml

--- remediation/remediationOnboarding.sh
+++ remediation/remediationOnboarding.sh
@@ -6,9 +6,16 @@ deployment_name="deploymentyael"
 # Set the YAML file name
 yaml_file="test.yaml"
 
-echo "Creating deployment..."
-if gcloud deployment-manager deployments update $deployment_name --config $yaml_file; then
+echo "Enabling Deployment Manager APIs, which you will need for this deployment."
+gcloud services enable deploymentmanager.googleapis.com
+
+echo "Creating or updating deployment..."
+if gcloud deployment-manager deployments describe $deployment_name --format="value(name)" &> /dev/null; then
+  echo "Deployment exists. Updating..."
+  gcloud deployment-manager deployments update $deployment_name --config $yaml_file
   echo "Deployment updated successfully."
 else
-  echo "Deployment failed."
+  echo "Deployment does not exist. Creating..."
+  gcloud deployment-manager deployments create $deployment_name --config $yaml_file
+  echo "Deployment created successfully."
 fi
