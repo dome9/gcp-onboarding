0e034bbfb79c2a8fef8705326e1222be3e38f517
yael <yaelel@checkpoint.com>
2023-06-25 14:46:22 +0300

    updated script

--- remediation/remediationOnboarding.sh
+++ remediation/remediationOnboarding.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-
 # Set the deployment name
-deployment_name="yael-deployment"
+deployment_name="yael-deployment1"
 
 # Set the YAML file name
 yaml_file="test.yaml"
-
 
 echo "Enabling Deployment Manager APIs, which you will need for this deployment."
 gcloud services enable deploymentmanager.googleapis.com
 
 echo "Creating deployment..."
-gcloud deployment-manager deployments create $deployment_name --config $yaml_file
-
-echo "Deployment created successfully."
-
+if gcloud deployment-manager deployments create $deployment_name --config $yaml_file; then
+  echo "Deployment created successfully."
+else
+  echo "Deployment failed."
+fi
