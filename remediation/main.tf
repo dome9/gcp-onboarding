1e8b45e8dcb3d87c67446d12c1641fa41d4a8ec9
yael <yaelel@checkpoint.com>
2023-07-06 16:56:09 +0300

    changed yaml

--- remediation/main.tf
+++ remediation/main.tf
@@ -1,62 +1,59 @@
 # main.tf
 
 # Create the IAM role
-  resource "google_project_iam_custom_role" "yaelRole3" {
+resource "google_project_iam_custom_role" "yaelRole2" {
   role_id      = "yaelRole2"
   title        = "yaelRole2"
   description  = "Custom role with specific permissions"
   permissions = [
-  "cloudsql.instances.get",
-  "cloudsql.instances.update",
-  "compute.firewalls.delete",
-  "compute.instances.get",
-  "compute.instances.setLabels",
-  "compute.instances.stop",
-  "compute.instances.deleteAccessConfig",
-  "compute.networks.updatePolicy",
-  "compute.subnetworks.get",
-  "compute.subnetworks.setPrivateIpGoogleAccess",
-  "compute.subnetworks.update",
-  "container.clusters.update",
-  "gkemulticloud.awsNodePools.update",
-  "storage.buckets.getIamPolicy",
-  "storage.buckets.setIamPolicy",
+    "cloudsql.instances.get",
+    "cloudsql.instances.update",
+    "compute.firewalls.delete",
+    "compute.instances.get",
+    "compute.instances.setLabels",
+    "compute.instances.stop",
+    "compute.instances.deleteAccessConfig",
+    "compute.networks.updatePolicy",
+    "compute.subnetworks.get",
+    "compute.subnetworks.setPrivateIpGoogleAccess",
+    "compute.subnetworks.update",
+    "container.clusters.update",
+    "gkemulticloud.awsNodePools.update",
+    "storage.buckets.getIamPolicy",
+    "storage.buckets.setIamPolicy",
   ]
   project = "chkp-gcp-yaelel-box"
 }
 
-  # Create the Cloud Storage bucket
-  resource "google_storage_bucket" "yaelBucket3" {
-  name     = "yael-test-3"
+# Create the Cloud Storage bucket
+resource "google_storage_bucket" "yaelBucket1" {
+  name     = "yael-test-1"
   project  = "chkp-gcp-yaelel-box"
 
-  uniform_bucket_level_access {
-  enabled = true
-  }
-
-  iam_configuration {
-  bucket_policy_only = true
-  }
+  uniform_bucket_level_access = true
 }
 
-  # Grant objectCreator role to allUsers on the bucket
-  resource "google_storage_bucket_iam_member" "yaelBucket3AllUsers" {
-  bucket = google_storage_bucket.yaelBucket3.name
+# Grant objectCreator role to allUsers on the bucket
+resource "google_storage_bucket_iam_binding" "yaelBucket1AllUsers" {
+  bucket = google_storage_bucket.yaelBucket1.name
   role   = "roles/storage.objectCreator"
-  member = "allUsers"
+
+  members = [
+    "allUsers",
+  ]
 }
 
-  # Create the IAM service account
-  resource "google_service_account" "yaelServiceAccount3" {
-  account_id   = "yaelServiceAccount3"
-  display_name = "yaelServiceAccount3"
+# Create the IAM service account
+resource "google_service_account" "yaelServiceAccount1" {
+  account_id   = "yael-service-account-1"
+  display_name = "yaelServiceAccount1"
 }
 
-  # Create the Cloud Function
-  resource "google_cloudfunctions_function" "yaelFunction3" {
-  name         = "yaelFunction3"
+# Create the Cloud Function
+resource "google_cloudfunctions_function" "yaelFunction1" {
+  name         = "yaelFunction1"
   runtime      = "python37"
-  source_archive_bucket = google_storage_bucket.yaelBucket3.name
+  source_archive_bucket = google_storage_bucket.yaelBucket1.name
   source_archive_object = "yael.zip"
   project      = "chkp-gcp-yaelel-box"
   location     = "us-central1"
@@ -67,6 +64,6 @@
   ingress_settings = "ALLOW_ALL"
 
   environment_variables = {
-  "SOURCE_ZIP_FILE" = "gs://${google_storage_bucket.yaelBucket3.name}/yael.zip"
+    "SOURCE_ZIP_FILE" = "gs://${google_storage_bucket.yaelBucket1.name}/yael.zip"
   }
 }
