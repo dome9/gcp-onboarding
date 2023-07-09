6f5c0149fe50528fe52ba800b0beddd5f5c88771
yael <yaelel@checkpoint.com>
2023-07-09 15:57:30 +0300

    changed yaml

--- remediation/main.tf
+++ remediation/main.tf
@@ -5,10 +5,10 @@ data "google_storage_bucket" "existing_yaelBucket1" {
 }
 
 resource "google_project_iam_custom_role" "yaelRole2" {
-  count        = length(data.google_storage_bucket.existing_yaelBucket1) > 0 ? 0 : 1
-  role_id      = "yaelRole2"
-  title        = "yaelRole2"
-  description  = "Custom role with specific permissions"
+  count       = length(data.google_storage_bucket.existing_yaelBucket1) > 0 ? 0 : 1
+  role_id     = "yaelRole2"
+  title       = "yaelRole2"
+  description = "Custom role with specific permissions"
   permissions = [
     "cloudsql.instances.get",
     "cloudsql.instances.update",
@@ -37,7 +37,7 @@ resource "google_storage_bucket" "yaelBucket1" {
 }
 
 resource "google_storage_bucket_iam_binding" "yaelBucket1AllUsers" {
-  count   = length(data.google_storage_bucket.existing_yaelBucket1) > 0 ? 0 : 1
+  count   = length(data.google_storage_bucket.existing_yaelBucket1) > 0 ? 1 : 0
   bucket  = google_storage_bucket.yaelBucket1[0].name
   role    = "roles/storage.objectCreator"
   members = ["allUsers"]
@@ -49,14 +49,14 @@ resource "google_service_account" "yaelServiceAccount1" {
 }
 
 resource "google_cloudfunctions_function" "yaelFunction1" {
-  count                  = length(data.google_storage_bucket.existing_yaelBucket1) > 0 ? 0 : 1
-  name                   = "yaelFunction1"
-  runtime                = "python37"
-  source_archive_bucket  = google_storage_bucket.yaelBucket1[0].name
-  source_archive_object  = "yael.zip"
-  region                 = "us-central1"  # Specify the desired region for the Cloud Function
-  entry_point            = "main"
-  service_account_email  = google_service_account.yaelServiceAccount1.email
+  count                 = length(data.google_storage_bucket.existing_yaelBucket1) > 0 ? 1 : 0
+  name                  = "yaelFunction1"
+  runtime               = "python37"
+  source_archive_bucket = google_storage_bucket.yaelBucket1[0].name
+  source_archive_object = "yael.zip"
+  region                = "us-central1"  # Specify the desired region for the Cloud Function
+  entry_point           = "main"
+  service_account_email = google_service_account.yaelServiceAccount1.email
 
   event_trigger {
     event_type = "google.storage.object.finalize"
