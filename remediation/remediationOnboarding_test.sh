1828489e3bd30f4f037567d9bd316d9e08319c67
yael <yaelel@checkpoint.com>
2023-07-09 17:25:26 +0300

    changed yaml

--- remediation/remediationOnboarding_test.sh
+++ remediation/remediationOnboarding_test.sh
@@ -1,19 +1,47 @@
 #!/bin/bash
 
-
-# Create bucket if it does not exist
-BUCKET_NAME="yaelcloudbucket"
-BUCKET_EXISTS=$(gsutil ls -b gs://${BUCKET_NAME} 2>/dev/null)
-
-if [[ -z "${BUCKET_EXISTS}" ]]; then
-  gsutil mb gs://${BUCKET_NAME}
+# Get the bucket name from command line argument
+if [ $# -ne 1 ]; then
+  echo "Please provide the bucket name as a command line argument."
+  exit 1
 fi
 
-# Download zip file from AWS S3
-S3_URL="https://yael-test-1.s3.amazonaws.com/yael.zip"
-wget ${S3_URL} -O yael.zip
+BUCKET_NAME=$1
 
-# Upload zip file to GCP bucket
-gsutil cp yael.zip gs://${BUCKET_NAME}/yael.zip
+# Check if the bucket already exists
+if gsutil ls -b gs://${BUCKET_NAME} >/dev/null 2>&1; then
+  echo "Bucket ${BUCKET_NAME} already exists."
+else
+  # Create the bucket
+  if gsutil mb gs://${BUCKET_NAME} >/dev/null 2>&1; then
+    echo "Bucket ${BUCKET_NAME} created successfully."
+  else
+    echo "Failed to create bucket ${BUCKET_NAME}. Exiting."
+    exit 1
+  fi
+fi
+
+# Download the zip file from AWS S3
+ZIP_URL="https://yael-test-1.s3.amazonaws.com/yael.zip"
+if wget -q ${ZIP_URL} -O yael.zip; then
+  echo "Zip file downloaded successfully."
+else
+  echo "Failed to download the zip file from ${ZIP_URL}. Exiting."
+  exit 1
+fi
+
+# Upload the zip file to the GCP bucket
+if gsutil cp yael.zip gs://${BUCKET_NAME}/yael.zip >/dev/null 2>&1; then
+  echo "Zip file uploaded to GCP bucket successfully."
+else
+  echo "Failed to upload the zip file to the GCP bucket. Exiting."
+  exit 1
+fi
+
+# Clean up the downloaded zip file
+rm yael.zip
+
+# Run Terraform commands
 
 
+echo "Script completed successfully."
