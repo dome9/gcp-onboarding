d5bf72305ecb1b2cb88e55cefe2fb008c68725b7
yael <yaelel@checkpoint.com>
2023-07-09 16:06:23 +0300

    changed yaml

--- remediation/main.tf
+++ remediation/main.tf
@@ -1,9 +1,9 @@
-data "google_storage_bucket_iam_policy" "existing_yaelBucket1" {
+data "google_storage_bucket" "existing_yaelBucket1" {
   bucket = "yael-test-1"
 }
 
 resource "google_project_iam_custom_role" "yaelRole2" {
-  count       = can(data.google_storage_bucket_iam_policy.existing_yaelBucket1.policy) ? 0 : 1
+  count       = length(data.google_storage_bucket.existing_yaelBucket1) > 0 ? 0 : 1
   role_id     = "yaelRole2"
   title       = "yaelRole2"
   description = "Custom role with specific permissions"
@@ -27,7 +27,7 @@ resource "google_project_iam_custom_role" "yaelRole2" {
 }
 
 resource "google_storage_bucket" "yaelBucket1" {
-  count    = can(data.google_storage_bucket_iam_policy.existing_yaelBucket1.policy) ? 0 : 1
+  count    = length(data.google_storage_bucket.existing_yaelBucket1) > 0 ? 0 : 1
   name     = "yael-test-1"
   location = "us-central1"  # Specify the desired location for the bucket
 
@@ -35,7 +35,7 @@ resource "google_storage_bucket" "yaelBucket1" {
 }
 
 resource "google_storage_bucket_iam_binding" "yaelBucket1AllUsers" {
-  count   = can(data.google_storage_bucket_iam_policy.existing_yaelBucket1.policy) ? 1 : 0
+  count   = length(data.google_storage_bucket.existing_yaelBucket1) > 0 ? 1 : 0
   bucket  = google_storage_bucket.yaelBucket1[0].name
   role    = "roles/storage.objectCreator"
   members = ["allUsers"]
@@ -47,7 +47,7 @@ resource "google_service_account" "yaelServiceAccount1" {
 }
 
 resource "google_cloudfunctions_function" "yaelFunction1" {
-  count                 = can(data.google_storage_bucket_iam_policy.existing_yaelBucket1.policy) ? 1 : 0
+  count                 = length(data.google_storage_bucket.existing_yaelBucket1) > 0 ? 1 : 0
   name                  = "yaelFunction1"
   runtime               = "python37"
   source_archive_bucket = google_storage_bucket.yaelBucket1[0].name
