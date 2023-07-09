f339f7864b1f9b5f7d262a9f5478ca48253f3e88
yael <yaelel@checkpoint.com>
2023-07-09 19:51:32 +0300

    changed yaml

--- remediation/main.tf
+++ remediation/main.tf
@@ -19,44 +19,65 @@ resource "google_project_iam_custom_role" "yaelRole2" {
     "storage.buckets.getIamPolicy",
     "storage.buckets.setIamPolicy",
   ]
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [
+      permissions,
+    ]
+  }
 }
 
+# Define the bucket name as a variable
 variable "bucket_name" {
   description = "The name of the GCP bucket"
   type        = string
 }
 
-resource "google_storage_bucket" "yaelBucket1" {
-  name     = var.bucket_name
-  location = "us-central1"  # Specify the desired location for the bucket
-
-  uniform_bucket_level_access = true
-}
-
+# Define the IAM binding for allUsers
 resource "google_storage_bucket_iam_binding" "yaelBucket1AllUsers" {
-  bucket  = google_storage_bucket.yaelBucket1.name
+  bucket  = var.bucket_name
   role    = "roles/storage.objectCreator"
   members = ["allUsers"]
 }
 
-resource "google_service_account" "yaelServiceAccount1" {
-  account_id   = "yael-service-account-1"
-  display_name = "yaelServiceAccount1"
+data "google_service_account" "existing_service_account" {
+  account_id = "yael-service-account-1"
 }
 
-resource "google_cloudfunctions_function" "yaelFunction1" {
-  name                  = "yaelFunction1"
-  runtime               = "python37"
-  source_archive_bucket = var.bucket_name
-  source_archive_object = "yael.zip"
-  region                = "us-central1"  # Specify the desired region for the Cloud Function
-  entry_point           = "main"
-  service_account_email = google_service_account.yaelServiceAccount1.email
+resource "google_service_account" "yaelServiceAccount1" {
+  count         = data.google_service_account.existing_service_account ? 0 : 1
+  account_id    = "yael-service-account-1"
+  display_name  = "yaelServiceAccount1"
+}
 
-  event_trigger {
-    event_type = "google.storage.object.finalize"
-    resource   = var.bucket_name
-  }
+# Define the existing Cloud Function
+data "google_cloudfunctions_function" "existing_function" {
+   name   = "yaelFunction1"
+   region = data.google_region.current.name
+ }
 
-  ingress_settings = "ALLOW_ALL"
+ resource "google_cloudfunctions_function" "yaelFunction1" {
+   count                 = data.google_cloudfunctions_function.existing_function ? 0 : 1
+   name                  = "yaelFunction1"
+   runtime               = "python37"
+   source_archive_bucket = var.bucket_name
+   source_archive_object = "yael.zip"
+   region                = data.google_region.current.name
+   entry_point           = "main"
+   service_account_email = google_service_account.yaelServiceAccount1.email
+
+   event_trigger {
+     event_type = "google.storage.object.finalize"
+     resource   = var.bucket_name
+   }
+
+   ingress_settings = "ALLOW_ALL"
+ }
+
+# Define the IAM binding for the Cloud Function and the custom role
+resource "google_cloudfunctions_function_iam_binding" "yaelFunction1Binding" {
+  function_name = google_cloudfunctions_function.yaelFunction1[*].name
+  role          = google_project_iam_custom_role.yaelRole2.role_id
+  members       = ["serviceAccount:${google_service_account.yaelServiceAccount1.email}"]
 }
