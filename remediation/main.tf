4c23021dfacd046ef633d3ea22cb8de5c9274ddb
yael <yaelel@checkpoint.com>
2023-07-09 20:00:07 +0300

    changed yaml

--- remediation/main.tf
+++ remediation/main.tf
@@ -51,33 +51,39 @@ resource "google_service_account" "yaelServiceAccount1" {
   display_name  = "yaelServiceAccount1"
 }
 
+# Connect the custom role to the service account
+resource "google_project_iam_binding" "yaelRole2Binding" {
+  project = google_project.project.project_id
+  role    = google_project_iam_custom_role.yaelRole2.role_id
+  members = ["serviceAccount:${google_service_account.yaelServiceAccount1.email}"]
+}
+
 # Define the existing Cloud Function
 data "google_cloudfunctions_function" "existing_function" {
-   name   = "yaelFunction1"
-   region = data.google_region.current.name
- }
+  name   = "yaelFunction1"
+  region = data.google_compute_region.current.name
+}
 
- resource "google_cloudfunctions_function" "yaelFunction1" {
-   count                 = data.google_cloudfunctions_function.existing_function ? 0 : 1
-   name                  = "yaelFunction1"
-   runtime               = "python37"
-   source_archive_bucket = var.bucket_name
-   source_archive_object = "yael.zip"
-   region                = data.google_region.current.name
-   entry_point           = "main"
-   service_account_email = google_service_account.yaelServiceAccount1.email
+resource "google_cloudfunctions_function" "yaelFunction1" {
+  count                 = data.google_cloudfunctions_function.existing_function ? 0 : 1
+  name                  = "yaelFunction1"
+  runtime               = "python37"
+  source_archive_bucket = var.bucket_name
+  source_archive_object = "yael.zip"
+  region                = data.google_compute_region.current.name
+  entry_point           = "main"
+  service_account_email = google_service_account.yaelServiceAccount1.email
 
-   event_trigger {
-     event_type = "google.storage.object.finalize"
-     resource   = var.bucket_name
-   }
+  event_trigger {
+    event_type = "google.storage.object.finalize"
+    resource   = var.bucket_name
+  }
 
-   ingress_settings = "ALLOW_ALL"
- }
+  ingress_settings = "ALLOW_ALL"
+}
 
 # Define the IAM binding for the Cloud Function and the custom role
 resource "google_cloudfunctions_function_iam_binding" "yaelFunction1Binding" {
-  function_name = google_cloudfunctions_function.yaelFunction1[*].name
-  role          = google_project_iam_custom_role.yaelRole2.role_id
+  function_name = google_cloudfunctions_function.yaelFunction1[0].name
   members       = ["serviceAccount:${google_service_account.yaelServiceAccount1.email}"]
 }
