424623b9bb040036b00dbb8580407af48efa680d
yael <yaelel@checkpoint.com>
2023-07-10 14:05:24 +0300

    changed yaml

--- remediation/main.tf
+++ remediation/main.tf
@@ -46,7 +46,8 @@ resource "google_service_account" "yael_service_account" {
 
 resource "google_project_iam_binding" "service_role_binding" {
   role = "projects/${data.google_project.project.project_id}/roles/${google_project_iam_custom_role.yaelRole2.role_id}"
-  project = data.google_project.project.project_id
+  /* project = data.google_project.project.project_id */
+  project = "chkp-gcp-yaelel-box"
   members = [
     "serviceAccount:${google_service_account.yael_service_account.email}",
   ]
