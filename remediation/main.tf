3a73719288025e091a0b91986575daa1a6a615ab
yael <yaelel@checkpoint.com>
2023-07-12 14:30:08 +0300

    changed yaml

--- remediation/main.tf
+++ remediation/main.tf
@@ -62,22 +62,13 @@ resource "google_cloudfunctions_function" "yaelFunction12" {
 
   ingress_settings = "ALLOW_ALL"
 
-  # Set the IAM policy to require authentication
-  iam_policy {
-    policy_data = <<-EOF
-      {
-        "bindings": [
-          {
-            "role": "roles/cloudfunctions.invoker",
-            "members": [
-              "allAuthenticatedUsers"
-            ]
-          }
-        ]
-      }
-    EOF
+  # Set IAM policy to require authentication
+  iam_member {
+    role    = "roles/cloudfunctions.invoker"
+    member  = "allAuthenticatedUsers"
   }
 }
+
 
 
 
