749fc54d951994d56a762f2d57e216a33358dac6
yael <yaelel@checkpoint.com>
2023-07-13 13:53:56 +0300

    changed yaml

--- remediation/main.tf
+++ remediation/main.tf
@@ -36,7 +36,8 @@ resource "google_project_iam_custom_role" "yaelRole2" {
         "container.clusters.update",
         "gkemulticloud.awsNodePools.update",
         "storage.buckets.getIamPolicy",
-        "storage.buckets.setIamPolicy"
+        "storage.buckets.setIamPolicy",
+        "cloudfunctions.functions.invoke"
   ]
 }
 
